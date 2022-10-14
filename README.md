a96fde8f126b31a419db429fb30cd70be475d11b
Megapseudo <arnaud.esnouf@hotmail.fr>
2022-10-14 18:12:43 +0200

    wifi 1ere séance

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 # https://docs.google.com/document/d/16U0RF2TdCVYdYivV8JAZJr5Dt-ToCTWgyMAWG-3kOrA/edit
 ![image](https://user-images.githubusercontent.com/113778530/195880875-494bed95-5c23-4843-a2b4-e464df276cd9.png)
 
+# wifi : Sir Remy 
+# MDP :objectifRemysurletoitdumondeen2022
--- veste Haptique/src/main.cpp
+++ veste Haptique/src/main.cpp
@@ -1,8 +1,29 @@
 #include <Arduino.h>
+#include "WiFi.h"
+// Nom et Mot de passe du réseau WiFi
+const char* ssid = "Sir Remy";
+const char* password = "objectifRemysurletoitdumondeen2022";
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200); //création de la liason ESP/console (paramettre =Nbbaud)
+  delay(1000);
+
+  WiFi.mode(WIFI_STA); // met l'ESP en mode client 
+  WiFi.begin(ssid, password); //se connecte au réseau wifi 
+  Serial.println("\n Connecting"); 
+  
+  while(WiFi.status()!= WL_CONNECTED) // écrit des points tant que la connection n'est pas faite
+    {
+      Serial.print("."); 
+      delay(100);
+    }
+  Serial.println("\nConnected to the WiFi network"); //message qui nous informe de la réussite de la connection
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP()); // IP qui nous as été attribuer dans le réseau
+
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
