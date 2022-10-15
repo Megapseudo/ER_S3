ad54504192552b54f38639851652f5bf69cceb16
Megapseudo <arnaud.esnouf@hotmail.fr>
2022-10-15 16:30:04 +0200

    changement de parametres

--- veste Haptique/src/main.cpp
+++ veste Haptique/src/main.cpp
@@ -6,7 +6,7 @@ const char* ssid = "Sir Remy";
 const char* password = "objectifRemysurletoitdumondeen2022";
 */
 const char* ssid = "freebox-3336AD";
-const char* password = "nkh94q2hbtqmrwxvqwhbrd";
+const char* password = "mkh94q2hbtqmrwxvqwhbrd";
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,7 +20,7 @@ void setup() {
   while(WiFi.status()!= WL_CONNECTED) // écrit des points tant que la connection n'est pas faite
     {
       Serial.print("."); 
-      delay(100);
+      delay(1000);
     }
   Serial.println("\nConnected to the WiFi network"); //message qui nous informe de la réussite de la connection
   Serial.print("Local ESP32 IP: ");
