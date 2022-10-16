c1619fa8aa28d818b802ef9ed9dbb1ad77a2cc4b
Megapseudo <arnaud.esnouf@hotmail.fr>
2022-10-16 17:02:54 +0200

    connection au Wifi fonctionnel

--- veste Haptique/src/main.cpp
+++ veste Haptique/src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
 #include "WiFi.h"
 // Nom et Mot de passe du réseau WiFi
-/*
+/* réseau E&R :
 const char* ssid = "Sir Remy";
 const char* password = "objectifRemysurletoitdumondeen2022";
+  réseau maison
+const char* ssid = "F reebox-3336AD";
+const char* password = "nkh94q2hbtqmrwxvqwhbrd";
+réseau partage de co
+const char* ssid = "Keg Smash";
+const char* password = "12345678";
 */
-const char* ssid = "freebox-3336AD";
-const char* password = "mkh94q2hbtqmrwxvqwhbrd";
+const char* ssid = "Freebox-3336AD";
+const char* password = "nkh94q2hbtqmrwxvqwhbrd";
+char tempo;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,12 +22,18 @@ void setup() {
 
   WiFi.mode(WIFI_STA); // met l'ESP en mode client 
   WiFi.begin(ssid, password); //se connecte au réseau wifi 
-  Serial.println("\n Connecting"); 
+  Serial.println("\n Connecting..."); 
   
   while(WiFi.status()!= WL_CONNECTED) // écrit des points tant que la connection n'est pas faite
     {
       Serial.print("."); 
+      tempo++;
       delay(1000);
+      if (tempo>=15)
+      {
+        Serial.println(WiFi.status());
+        tempo = 0;
+      }
     }
   Serial.println("\nConnected to the WiFi network"); //message qui nous informe de la réussite de la connection
   Serial.print("Local ESP32 IP: ");
