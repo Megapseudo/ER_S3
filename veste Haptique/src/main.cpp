0e0bb7efceedebae9aa27d15370fca77c8865051
Megapseudo <arnaud.esnouf@hotmail.fr>
2022-12-09 17:03:17 +0100

    delete

--- veste Haptique/src/main.cpp
+++ veste Haptique/src/main.cpp
@@ -1,22 +1,31 @@
 #include <Arduino.h>
 #include "WiFi.h"
 #include "WebSocketsServer.h"
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
 
 char tempo;
+// déclaration des fonctions d'initialisation et des tâches pour le RTOS
 void init_connection(); 
 void TaskTCPlink( void *pvParameters );
 void TaskTORlink( void *pvParameters );
+// déclaration des objets 
 QueueHandle_t xQueue = xQueueCreate( 100000, sizeof( char) );
 BaseType_t status;
+WebSocketsServer webSocket = WebSocketsServer(81);
+
 
 
 void setup() {  
+ 
   //put your setup code here, to run once:
   //launch wifi connection with ssid and password
    init_connection();
   //création des deux taches qu'on va utiliser
   xTaskCreate(TaskTCPlink,"TaskTPClink",1000,NULL,1 ,NULL);
   xTaskCreate(TaskTORlink,"TaskTORlink",1000,NULL,1,NULL);
+   
  }
 
 
@@ -31,14 +40,14 @@ void init_connection()
 const char* ssid = "Routeur Maison";
 const char* password = "objectifRemysurletoitdumondeen2022";
   //réseau maison
-const char* ssid = "F reebox-3336AD";
-const char* password = "nkh94q2hbtqmrwxvqwhbrd";
+//const char* ssid = "Freebox-3336AD";
+//const char* password = "nkh94q2hbtqmrwxvqwhbrd";
  // réseau partage de co
-const char* ssid = "Keg Smash";
-const char* password = "12345678";
+//const char* ssid = "Keg Smash";
+//const char* password = "12345678";
 
 
-  Serial.begin(9600); //création de la liason ESP/console (paramettre =Nbbaud)
+  Serial.begin(115200); //création de la liason ESP/console (paramettre =Nbbaud)
   delay(1000);
 
   WiFi.mode(WIFI_STA); // met l'ESP en mode client 
@@ -64,10 +73,10 @@ const char* password = "12345678";
 }
 void TaskTPClink(void*pvParameters)
 {
-  
+  webSocket.begin();
 while(1)
 {
-  
+  //webSocket.onEvent(webSocketEvent);
 }
 
 }
@@ -118,4 +127,57 @@ while(1)
 }
 }
 
+// void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+
+//     switch(type) {
+//         case WStype_DISCONNECTED:
+//             Serial.printf("[%u] Disconnected!\n", num);
+//             break;
+//         case WStype_CONNECTED:
+//             {
+//                 IPAddress ip = WiFi.localIP();
+//                 Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+// 				// send message to client
+// 				webSocket.sendTXT(num, "Connected");
+//             }
+//             break;
+//         case WStype_TEXT:
+//             Serial.printf("[%u] get Text: %s\n", num, payload);
+
+//             // send message to client
+//             // webSocket.sendTXT(num, "message here");
+
+//             // send data to all connected clients
+//             // webSocket.broadcastTXT("message here");
+//             break;
+//         case WStype_BIN:
+//             Serial.printf("[%u] get binary length: %u\n", num, length);
+//             hexdump(payload, length);
+
+//             // send message to client
+//             // webSocket.sendBIN(num, payload, length);
+//             break;
+// 		case WStype_ERROR:			
+// 		case WStype_FRAGMENT_TEXT_START:
+// 		case WStype_FRAGMENT_BIN_START:
+// 		case WStype_FRAGMENT:
+// 		case WStype_FRAGMENT_FIN:
+// 			break;
+//     }
+
+// }
+
+// void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+// 	const uint8_t* src = (const uint8_t*) mem;
+// 	Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+// 	for(uint32_t i = 0; i < len; i++) {
+// 		if(i % cols == 0) {
+// 			Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+// 		}
+// 		Serial.printf("%02X ", *src);
+// 		src++;
+// 	}
+//   Serial.printf("\n");
+// }
 
